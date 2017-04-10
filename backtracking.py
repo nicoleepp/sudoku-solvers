db3f8f85d4545c6f3485ba9841d3b06ab773bd8d
Kiera Enns <keenns12@hotmail.com>
2017-04-10 12:16:13 -0500

    improvements

--- backtracking.py
+++ backtracking.py
@@ -1,5 +1,12 @@
 # Source: http://stackoverflow.com/questions/1697334/algorithm-for-solving-sudoku
 
+import sys
+
+def output(a):
+    sys.stdout.write(str(a))
+
+N = 9
+
 field = [[5,1,7,6,0,0,0,3,4],
          [2,8,9,0,0,4,0,0,0],
          [3,4,6,2,0,5,0,9,0],
@@ -10,6 +17,17 @@ field = [[5,1,7,6,0,0,0,3,4],
          [7,0,3,4,0,0,5,6,0],
          [0,0,0,0,0,0,0,0,0]]
 
+field2 = [[0,0,0,2,6,0,7,0,1],
+          [6,8,0,0,7,0,0,9,0],
+          [1,9,0,0,0,4,5,0,0],
+          [8,2,0,1,0,0,0,4,0],
+          [0,0,4,6,0,2,9,0,0],
+          [0,5,0,0,0,3,0,2,8],
+          [0,0,9,3,0,0,0,7,4],
+          [0,4,0,0,5,0,0,3,6],
+          [7,0,3,0,1,8,0,0,0]]
+
+# Finds next empty cell, returns that location
 def findNextCellToFill(grid, i, j):
     for x in range(i,9):
         for y in range(j,9):
@@ -26,7 +44,7 @@ def isValid(grid, i, j, e):
     if rowOk:
         columnOk = all([e != grid[x][j] for x in range(9)])
         if columnOk:
-            # finding the top left x,y co-ordinates of the section containing the i,j cell
+            # Finding the top left x,y co-ordinates of the section containing the i,j cell
             secTopX, secTopY = 3 *(i/3), 3 *(j/3)
             for x in range(secTopX, secTopX+3):
                 for y in range(secTopY, secTopY+3):
@@ -36,9 +54,8 @@ def isValid(grid, i, j, e):
     return False
 
 def solveSudoku(grid, i=0, j=0):
+    # Start at the beginning [0][0]
     i,j = findNextCellToFill(grid, i, j)
-    if i == -1:
-        return True
     for e in range(1,10):
         if isValid(grid,i,j,e):
             grid[i][j] = e
@@ -46,7 +63,30 @@ def solveSudoku(grid, i=0, j=0):
                 return True
             # Undo the current cell for backtracking
             grid[i][j] = 0
+    if i == -1:
+        return True
     return False
 
-print solveSudoku(field)
-print field
+def print_field(field):
+    if not field:
+        output("No solution")
+        return
+    for i in range(N):
+        for j in range(N):
+            cell = field[i][j]
+            if cell == 0 or isinstance(cell, set):
+                output('.')
+            else:
+                output(cell)
+            if (j + 1) % 3 == 0 and j < 8:
+                output(' |')
+
+            if j != 8:
+                output(' ')
+        output('\n')
+        if (i + 1) % 3 == 0 and i < 8:
+            output("- - - + - - - + - - -\n")
+        
+solveSudoku(field2)
+print_field(field2)
+
--- propagation_forwardtracking_with_lookahead.py
+++ propagation_forwardtracking_with_lookahead.py
@@ -19,6 +19,16 @@ field = [[5,1,7,6,0,0,0,3,4],
          [7,0,3,4,0,0,5,6,0],
          [0,0,0,0,0,0,0,0,0]]
 
+field2 = [[0,0,0,2,6,0,7,0,1],
+          [6,8,0,0,7,0,0,9,0],
+          [1,9,0,0,0,4,5,0,0],
+          [8,2,0,1,0,0,0,4,0],
+          [0,0,4,6,0,2,9,0,0],
+          [0,5,0,0,0,3,0,2,8],
+          [0,0,9,3,0,0,0,7,4],
+          [0,4,0,0,5,0,0,3,6],
+          [7,0,3,0,1,8,0,0,0]]
+
 def print_field(field):
     if not field:
         output("No solution")
@@ -40,7 +50,7 @@ def print_field(field):
             output("- - - + - - - + - - -\n")
 
 def read(field):
-    """ Read field into state (replace 0 with set of possible values) """
+    # Read field into state (replace 0 with set of possible values)
 
     state = deepcopy(field)
     for i in range(N):
@@ -55,7 +65,7 @@ state = read(field)
 
 
 def done(state):
-    """ Are we done? """
+    # Are we done?
 
     for row in state:
         for cell in row:
@@ -65,7 +75,7 @@ def done(state):
 
 
 def propagate_step(state):
-    """ Propagate one step """
+    # Propagate one step
 
     new_units = False
 
@@ -114,7 +124,7 @@ def propagate_step(state):
     return True, new_units
 
 def propagate(state):
-    """ Propagate until we reach a fixpoint """
+    # Propagate until we reach a fixpoint
     while True:
         solvable, new_unit = propagate_step(state)
         if not solvable:
@@ -124,7 +134,7 @@ def propagate(state):
 
 
 def solve(state):
-    """ Solve sudoku """
+    # Solve sudoku
 
     solvable = propagate(state)
 
