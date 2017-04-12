70b8111714676c5d0aa1e3cbce8565e8d0ff0f08
Kiera Enns <keenns12@hotmail.com>
2017-04-11 21:13:39 -0500

    fixed forward tracking algorithm and added another part to puzzles json of whether to include them in the run or not (since some break the algorithm)

--- run_algs.py
+++ run_algs.py
@@ -1,5 +1,5 @@
 from propagation_forwardtracking_with_lookahead import (
-    solve as lookahead_solver
+    sudokuSolver as lookahead_solver
 )
 from backtracking import sudokuSolver as backtracking_solver
 from simulated_annealing import sudoku_solver as sim_annealing_solver
@@ -8,23 +8,10 @@ from utils import print_sudoku_puzzle, validate_sudoku_solution
 import json
 import time
 
-# Designed to work against the brute-force algorithm:
-# Brute-force just seems to run forever and never conclude, so not actually including for now
-'''[
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 3, 0, 8, 5],
-    [0, 0, 1, 0, 2, 0, 0, 0, 0],
-    [0, 0, 0, 5, 0, 7, 0, 0, 0],
-    [0, 0, 4, 0, 0, 0, 1, 0, 0],
-    [0, 9, 0, 0, 0, 0, 0, 0, 0],
-    [5, 0, 0, 0, 0, 0, 0, 7, 3],
-    [0, 0, 2, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 4, 0, 0, 0, 9]
-]'''
-
 
 class Puzzle(object):
-    def __init__(self, type, puzzle):
+    def __init__(self, include, type, puzzle):
+        self.include = include
         self.type = type
         self.puzzle = puzzle
 
@@ -35,66 +22,67 @@ with open('puzzles.json') as data_file:
     puzzles = data["puzzles"]
 
     for puzzleObject in puzzles:
-        newPuzzle = Puzzle(puzzleObject["type"], puzzleObject["puzzle"])
-        puzzle = newPuzzle.puzzle
-        print("="*60 + "\n" + "="*60 + "\n")
-        print(
-            "Puzzle type: " + newPuzzle.type +
-            "\nInitial puzzle: \n"
-        )
-        print_sudoku_puzzle(puzzle)
-
-        # Backtracking solver
-        print ("Solving with backtracking: \n")
-        result = None
-
-        startTime = time.time() * 1000
-        result = backtracking_solver(puzzle)
-        endTime = time.time() * 1000
-        totalTime = endTime - startTime
-
-        if result is not None:
-            print_sudoku_puzzle(result)
-            print (
-                "\nThis solution is valid: " + str(validate_sudoku_solution(result)) +
-                "\nRuntime was: " + str(totalTime) + "ms" + "\n"
+        newPuzzle = Puzzle(puzzleObject["include"], puzzleObject["type"], puzzleObject["puzzle"])
+        if newPuzzle.include:
+            puzzle = newPuzzle.puzzle
+            print("="*60 + "\n" + "="*60 + "\n")
+            print(
+                "Puzzle type: " + newPuzzle.type +
+                "\nInitial puzzle: \n"
             )
-        else:
-            print("No solution found by backtracking algorithm")
+            print_sudoku_puzzle(puzzle)
 
-        # Lookadhead solver
-        print ("Solving with propagation_forwardtracking_with_lookadhead: \n")
-        result = None
+            # Backtracking solver
+            print ("Solving with backtracking: \n")
+            result = None
 
-        startTime = time.time() * 1000
-        result = lookahead_solver(puzzle)
-        endTime = time.time() * 1000
-        totalTime = endTime - startTime
+            startTime = time.time() * 1000
+            result = backtracking_solver(puzzle)
+            endTime = time.time() * 1000
+            totalTime = endTime - startTime
 
-        if result is not None:
-            print_sudoku_puzzle(result)
-            print (
-                "This solution is valid: " + str(validate_sudoku_solution(result)) + "\n" + 
-                "Runtime was: " + str(totalTime) + "ms" + "\n"
-            )
-        else:
-            print("No solution found by lookahead algorithm")
+            if result is not None:
+                print_sudoku_puzzle(result)
+                print (
+                    "\nThis solution is valid: " + str(validate_sudoku_solution(result)) +
+                    "\nRuntime was: " + str(totalTime) + "ms" + "\n"
+                )
+            else:
+                print("No solution found by backtracking algorithm")
 
-        # Sim annealing solver
-        print ("Solving with sim_annealing_solver: \n")
-        result = None
+            # Lookadhead solver
+            print ("Solving with propagation_forwardtracking_with_lookadhead: \n")
+            result = None
 
-        startTime = time.time() * 1000
-        result = sim_annealing_solver(puzzle)
-        endTime = time.time() * 1000
-        totalTime = endTime - startTime
+            startTime = time.time() * 1000
+            result = lookahead_solver(puzzle)
+            endTime = time.time() * 1000
+            totalTime = endTime - startTime
 
-        if result is not None:
-            print_sudoku_puzzle(result)
-            print (
-                "This solution is valid: " + str(validate_sudoku_solution(result)) +
-                "\nRuntime was: " + str(totalTime) + "ms" + "\n"
-            )
-        else:
-            print("No solution found by simulated annealing algorithm")
+            if result is not None:
+                print_sudoku_puzzle(result)
+                print (
+                    "This solution is valid: " + str(validate_sudoku_solution(result)) + "\n" + 
+                    "Runtime was: " + str(totalTime) + "ms" + "\n"
+                )
+            else:
+                print("No solution found by lookahead algorithm")
+
+            # Sim annealing solver
+            print ("Solving with sim_annealing_solver: \n")
+            result = None
+
+            startTime = time.time() * 1000
+            result = sim_annealing_solver(puzzle)
+            endTime = time.time() * 1000
+            totalTime = endTime - startTime
+
+            if result is not None:
+                print_sudoku_puzzle(result)
+                print (
+                    "This solution is valid: " + str(validate_sudoku_solution(result)) +
+                    "\nRuntime was: " + str(totalTime) + "ms" + "\n"
+                )
+            else:
+                print("No solution found by simulated annealing algorithm")
 
